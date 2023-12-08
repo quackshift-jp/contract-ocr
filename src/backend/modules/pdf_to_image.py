53c9b375ff0ef4fcbdc5cae2d565405caa47d4fd
apple <apple@appurunoMacBook-Pro.local>
2023-12-08 15:57:59 +0900

    change:streamlitのPDFオブジェクトをjpegに変換するメソッド実装

--- src/backend/modules/pdf_to_image.py
+++ src/backend/modules/pdf_to_image.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 from pdf2image import convert_from_path
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+from PIL import Image
+import tempfile
 
 
 def convert_pdf_to_images(
@@ -9,3 +12,13 @@ def convert_pdf_to_images(
         convert_from_path(
             pdf, output_folder=save_path, fmt="jpeg", output_file=pdf.stem, dpi=200
         )
+
+
+def convert_streamlit_pdf_to_images(pdf_file: UploadedFile) -> list[Image.Image]:
+    # ディスクに一時ファイルを書き出すことで、メモリ消費を抑える
+    with tempfile.NamedTemporaryFile() as temp_file:
+        temp_file.write(pdf_file.read())
+        temp_file_path = temp_file.name
+
+        images = convert_from_path(temp_file_path)
+    return images
