8114ce09477c21599b07823489f1c228014df893
apple <apple@appurunoMacBook-Pro.local>
2023-12-08 15:58:54 +0900

    change:GoogleVisionAPIの仕様をビジネスロジックに合わせた修正

--- src/backend/ocr/google_vision.py
+++ src/backend/ocr/google_vision.py
@@ -1,27 +1,45 @@
-from google.cloud import vision
+from typing import Union
 import io
-from google.cloud.vision_v1 import AnnotateImageResponse
 import json
+from PIL import Image
+
+from google.cloud import vision
+from google.cloud.vision_v1 import AnnotateImageResponse
+
 from backend.models.text_detecter import TextDetecter
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 
 class GoogleVisionDetecter(TextDetecter):
     def __init__(self, client):
         self.client = client
 
-    def read_image(self, image_path: str) -> vision.Image:
-        # TODO:try-exceptのエラーハンドリングも導入する
-        with io.open(image_path, "rb") as image_file:
-            content = image_file.read()
+    def read_image(self, jpeg_file: Union[UploadedFile, Image.Image]) -> vision.Image:
+        if isinstance(jpeg_file, UploadedFile):
+            content = jpeg_file.read()
+        else:
+            img_byte_arr = io.BytesIO()
+            jpeg_file.save(img_byte_arr, format="JPEG")
+            content = img_byte_arr.getvalue()
         return vision.Image(content=content)
 
     def extract_text(self, response: AnnotateImageResponse) -> str:
         json_response = json.loads(AnnotateImageResponse.to_json(response))
         return json_response["fullTextAnnotation"]["text"].replace("\n", "")
 
-    def detect_text(self, image_path: str) -> str:
-        image = self.read_image(image_path)
-        response = self.client.document_text_detection(
-            image=image, image_context={"language_hints": ["ja"]}
-        )
-        return self.extract_text(response)
+    def detect_text(self, jpeg_file: UploadedFile) -> str:
+        if isinstance(jpeg_file, UploadedFile):
+            image = self.read_image(jpeg_file)
+            response = self.client.document_text_detection(
+                image=image, image_context={"language_hints": ["ja"]}
+            )
+            return self.extract_text(response)
+        texts = []
+        for file in jpeg_file:
+            image = self.read_image(file)
+            response = self.client.document_text_detection(
+                image=image, image_context={"language_hints": ["ja"]}
+            )
+            text = self.extract_text(response)
+            texts.append(text)
+        return "\n".join(texts)
