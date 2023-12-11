19c90321f3590f7641c848feff87098b201835d4
apple <apple@appurunoMacBook-Pro.local>
2023-12-11 17:29:27 +0900

    delete:不要なメソッドを削除

--- src/backend/modules/pdf_to_image.py
+++ src/backend/modules/pdf_to_image.py
@@ -5,15 +5,6 @@ from PIL import Image
 import tempfile
 
 
-def convert_pdf_to_images(
-    google_path: Path, save_path: Path, search_keyword: str
-) -> None:
-    for pdf in google_path.glob(f"*{search_keyword}*"):
-        convert_from_path(
-            pdf, output_folder=save_path, fmt="jpeg", output_file=pdf.stem, dpi=200
-        )
-
-
 def convert_streamlit_pdf_to_images(pdf_file: UploadedFile) -> list[Image.Image]:
     # ディスクに一時ファイルを書き出すことで、メモリ消費を抑える
     with tempfile.NamedTemporaryFile() as temp_file:
