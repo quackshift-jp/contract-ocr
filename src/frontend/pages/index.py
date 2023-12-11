e45ceb223ed1efd650addbd41cbe4273f263c0bd
apple <apple@appurunoMacBook-Pro.local>
2023-12-11 16:05:20 +0900

    streamlitへ項目抽出メソッドを簡易的に組み込む

--- src/frontend/pages/index.py
+++ src/frontend/pages/index.py
@@ -8,7 +8,7 @@ sys.path.append(str(pathlib.Path().absolute()))
 from backend.modules.pdf_to_image import (
     convert_streamlit_pdf_to_images,
 )
-from backend.main import detect
+from backend.main import detect, extract_items
 
 
 def index_page() -> None:
@@ -25,7 +25,19 @@ def index_page() -> None:
         st.image(file)
 
         response = detect(file)
-        st.write(response)
+        # TODO:フロントの責務を分離する
+        contract_items = extract_items(response)
+
+        # JSONオブジェクトの各キーと値を表示し、編集可能にする
+        edited_json = {}
+        for key, value in contract_items["content"].items():
+            new_value = st.sidebar.text_input(f"{key}: ", value)
+            edited_json[key] = new_value
+
+        # 編集された内容を表示するボタン
+        if st.sidebar.button("更新"):
+            st.sidebar.write("更新されたJSON:")
+            st.sidebar.json(edited_json)
 
 
 index_page()
