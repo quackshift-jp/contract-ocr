6dda378fd91d707e3b214bd67cf10dbb9fcfb4db
apple <apple@appurunoMacBook-Pro.local>
2023-12-11 17:30:37 +0900

    change:OCRメソッドのType修正と、タイポを修正する

--- src/backend/models/text_detecter.py
+++ src/backend/models/text_detecter.py
@@ -1,26 +1,30 @@
 from abc import abstractmethod, ABC
+from typing import Union
+from PIL import Image
+
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 from google.cloud import vision
 
 
-class TextDetecter(ABC):
+class TextDetector(ABC):
     def __init__(self, client: vision.ImageAnnotatorClient):
         self.client = client
 
     @abstractmethod
-    def read_image(self, image_path: str) -> any:
+    def read_image(self, jpeg_file: Union[UploadedFile, Image.Image]) -> vision.Image:
         """画像の読み込みメソッド
         args:
-            image_path str: 読み込み対象となる画像が存在しているパス
+            jpeg_file Union[UploadedFile, Image.Image]: 読み込み対象となるjpeg画像
         return:
             any: 各APIのOCRメソッドに適した画像情報(Google Visionの場合は、vision.Image型を返す必要がある)
         """
         raise NotImplementedError()
 
     @abstractmethod
-    def detect_text(self, image_path: str) -> str:
+    def detect_text(self, jpeg_file: Union[UploadedFile, Image.Image]) -> str:
         """OCRのテキスト検知メソッド
         args:
-            image any: 各APIのOCRメソッドに適した画像情報
+            jpeg_file Union[UploadedFile, Image.Image]: 各APIのOCRメソッドに適したjpeg画像情報
         return:
             str: 識別された文字情報
         """
--- tests/models/test_text_detecter.py
+++ tests/models/test_text_detecter.py
@@ -1,14 +1,14 @@
-from backend.models.text_detecter import TextDetecter
+from backend.models.text_detecter import TextDetector
 
 
-class MyTestClass(TextDetecter):
+class MyTestClass(TextDetector):
     def __init__(self):
         pass
 
-    def read_image(self, image_path="テストパス"):
+    def read_image(self, jpeg_file="テストイメージ"):
         return "TextDetecterの継承1"
 
-    def detect_text(self, image="テストイメージ"):
+    def detect_text(self, jpeg_file="テストイメージ"):
         return "TextDetecterの継承2"
 
 
