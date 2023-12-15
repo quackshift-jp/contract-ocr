50a56cc64d41684cfd0c507103571ba323358c2f
apple <apple@appurunoMacBook-Pro.local>
2023-12-15 22:41:43 +0900

    change:ダッシュボードページの微修正

--- src/frontend/pages/dashboard.py
+++ src/frontend/pages/dashboard.py
@@ -4,9 +4,12 @@ from frontend.component.api.request import request_read_contract_endpoint
 
 
 def dashboard_page():
-    print("出力")
+    st.title("契約書管理ダッシュボード")
     contracts = request_read_contract_endpoint(
         endpoint="http://127.0.0.1:8000/get/contracts/"
     )
-    df = pd.DataFrame(contracts)
+    columns = list(contracts[0].keys())
+    sort_column = st.sidebar.selectbox("並び替えるカラムを選択してください", columns)
+    df = pd.DataFrame(contracts).sort_values(by=sort_column)
+
     st.dataframe(df)
