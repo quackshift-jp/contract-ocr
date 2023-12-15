d0757fd4e7c827d6bfa4d95cea6436088d8073a0
apple <apple@appurunoMacBook-Pro.local>
2023-12-16 00:05:09 +0900

    add:insertメソッドの修正

---
+++ src/backend/router/insert.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from backend.crud import insert
+from backend.schemas.schemas import Contract
+from backend.db import db
+
+router = APIRouter()
+
+
+@router.post("/insert/contracts", response_model=Contract)
+async def insert_contracts_endpoint(
+    contract_id: int,
+    contractor: str,
+    db: Session = Depends(db.get_db_session),
+) -> list[Contract]:
+    result = insert.insert_contract(contract_id, contractor, db=db)
+    return result
