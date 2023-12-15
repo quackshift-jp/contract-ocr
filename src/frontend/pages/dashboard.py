a837561159612586903b798ef2e9d8dda95d3d04
apple <apple@appurunoMacBook-Pro.local>
2023-12-15 14:45:14 +0900

    delete:不要スクリプトの削除

--- src/frontend/pages/dashboard.py
+++ src/frontend/pages/dashboard.py
@@ -4,7 +4,6 @@ from frontend.component.api.request import request_read_contract_endpoint
 
 
 def dashboard_page():
-    print("出力")
     contracts = request_read_contract_endpoint(
         endpoint="http://127.0.0.1:8000/get/contracts/"
     )
