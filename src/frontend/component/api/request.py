8a1be37721f82e2acc4def207e86088a51ab2b22
apple <apple@appurunoMacBook-Pro.local>
2023-12-16 16:21:37 +0900

    add:契約書データの書き込みリクエストを実装

--- src/frontend/component/api/request.py
+++ src/frontend/component/api/request.py
@@ -6,3 +6,12 @@ def request_read_contract_endpoint(endpoint: str) -> list[dict[str, any]]:
     if response.status_code == 200:
         json_data = response.json()
         return json_data
+
+
+def request_insert_contract_endpoint(
+    endpoint: str, contractor: str
+) -> list[dict[str, any]]:
+    response = requests.post(f"{endpoint}?contractor={contractor}")
+    if response.status_code == 200:
+        json_data = response.json()
+        return json_data
