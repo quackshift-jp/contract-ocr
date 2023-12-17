0e1bf89868d38f4a997189a171f9198b307ee77f
apple <apple@appurunoMacBook-Pro.local>
2023-12-17 22:23:18 +0900

    change:メソッドのエラーメッセージを修正

--- src/frontend/component/utils/navigation.py
+++ src/frontend/component/utils/navigation.py
@@ -4,8 +4,12 @@ import streamlit as st
 def next_file():
     if st.session_state["current_index"] < len(st.session_state["upload_files"]) - 1:
         st.session_state["current_index"] += 1
+    else:
+        st.error("これより次のファイルはありません")
 
 
 def previous_file():
     if st.session_state["current_index"] > 0:
         st.session_state["current_index"] -= 1
+    else:
+        st.error("これより前のファイルはありません")
