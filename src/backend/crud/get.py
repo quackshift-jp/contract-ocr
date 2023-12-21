b75143dca80c2cd05f4005ddd50a1a0dcf9b5c6d
apple <apple@appurunoMacBook-Pro.local>
2023-12-21 16:35:01 +0900

    change:getメソッドで、特定の行だけを読み込めるようにする

--- src/backend/crud/get.py
+++ src/backend/crud/get.py
@@ -1,6 +1,8 @@
+from typing import Union
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 from fastapi import HTTPException
+
 from backend.schemas.schemas import Contract
 
 
@@ -11,3 +13,17 @@ def get_contracts(db: Session) -> list[Contract]:
         raise HTTPException(status_code=404, detail="Table not found")
     contracts = [Contract(**row._asdict()) for row in result]
     return contracts
+
+
+def get_specific_contract(
+    column_name: str, column_value: str, db: Session
+) -> Union[None, Contract]:
+    sql = text(
+        f"""
+        SELECT * FROM contract WHERE {column_name} = '{column_value}';
+    """
+    )
+    result = db.execute(sql).first()
+    if result is None:
+        return None  # Return None if no data is found
+    return Contract(**result._asdict())
--- src/backend/router/get.py
+++ src/backend/router/get.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -13,4 +14,17 @@ async def get_contracts_endpoint(
     db: Session = Depends(db.get_db_session),
 ) -> list[Contract]:
     contracts = get.get_contracts(db=db)
+    return contracts
+
+
+@router.get("/get/contract", response_model=Union[None, Contract])
+async def get_specific_contract_endpoint(
+    column_name: str,
+    column_value: str,
+    db: Session = Depends(db.get_db_session),
+) -> Contract:
+    contracts = get.get_specific_contract(column_name, column_value, db=db)
+    print("タイプの出力")
+    print(type(contracts))
+    print(contracts)
     return contracts
